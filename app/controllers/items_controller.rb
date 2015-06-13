a1596a30c1c948358069f96278142199a612624e
Yasuhiro Morishita <yasuhiro.morishita0413@gmail.com>
2015-06-13 16:48:20 +0900

    add index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
   def show
     @item = Item.find(params[:id])
   end
 
-  def index
-    @items = Item.all
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'items/:id' => 'items#show'
+  get 'items' => 'items#index'
+  get 'items/new' => 'items#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
